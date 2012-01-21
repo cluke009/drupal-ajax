0a08322d91a7697bf877c35b0253fe0f91a8b160
Roni Kantis <roni.kantis@gmail.com>
2012-01-21 14:07:40 +0200

    Issue #718070 by davibrosk, bfr: Ajax redirect caused forms to submit multiple times

--- plugins/disable_redirect/ajax_disable_redirect.js
+++ plugins/disable_redirect/ajax_disable_redirect.js
@@ -6,22 +6,27 @@
  * @depends Drupal 6
  * @author brendoncrawford
  * @note This file uses a 79 character width limit.
- * 
+ *
  * @see http://drupal.org/node/114774#javascript-behaviors
  *
  */
 
 /**
  * Disables redirection for Ajax forms
- * 
+ *
  * @param {String} hook
  * @param {Object} args
  * @return {Bool}
  */
 Drupal.Ajax.plugins.disable_redirect = function(hook, args) {
+  if (hook === 'submit') {
+        args.submitter.attr('disabled', 'true');
+  }
+
   if (hook === 'redirect') {
     if (args.options.disable_redirect === true) {
       args.local.form[0].reset();
+          args.local.submitter.removeAttr('disabled');
       //IE
       $('.form-item select', args.local.form[0]).attr('selectedIndex', 0);
       if (args.options.remove_form === true) {
@@ -34,5 +39,4 @@ Drupal.Ajax.plugins.disable_redirect = function(hook, args) {
     }
   }
 }
-
 
